de984b514871d46add3b0642c9b65d933c8ff3e2
emmanuelkevins <emmanuelkevins24@gmail.com>
2025-06-21 21:13:21 +0800

    more

--- basics.c
+++ basics.c
@@ -1,11 +1,34 @@
-// Header file for input output functions
 #include <stdio.h>
+#include <string.h>
 
-// Main function: entry point for execution
+// //Struct
+//     struct student{
+//         int roll;
+//         char name[25];
+//         char dept[10];
+//         char address[50];
+//     };
+
 int main() {
 
-    // Writing print statement to print hello world
-    printf("Hello World");
+//Arrays
+    // int A[5];
+    // int B[] = {2,4,6,8,10};
+    // int i;
+    // for(i=0;i<(sizeof(B)/sizeof(B[0]));i++){
+    //     printf("%d\n",B[i]);
+    // }
+    // int C[10] = {1,2,3,4,5,6,7,8,9};
+
+    // struct student s1;
+    // s1.roll = 5;
+    // strcpy(s1.name, "John");
+    // strcpy(s1.dept, "math");
+    // strcpy(s1.address, "Singapore");
+
+    // printf("Roll: %d\nName: %s\nDept: %s\nAddress: %s\n",
+    //        s1.roll, s1.name, s1.dept, s1.address);
+    
 
     return 0;
 }
